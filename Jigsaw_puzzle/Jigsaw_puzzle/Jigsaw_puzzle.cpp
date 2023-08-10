ff712721c14dda235b5404e5c2d2adf81f85a647
lijunlin0 <117365903+lijunlin0@users.noreply.github.com>
2023-08-10 15:32:44 +0800

    项目初始化

---
+++ Jigsaw_puzzle/Jigsaw_puzzle/Jigsaw_puzzle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<graphics.h>
+#include<conio.h>
+using namespace std;
+const int WIDTH = 1000;
+const int HEIGHT = 1000;
+using namespace std;
+
+int main()
+{
+	initgraph(WIDTH, HEIGHT, SHOWCONSOLE);
+	setlinecolor(LIGHTBLUE);
+	system("pause");
+	return 0;
+}
